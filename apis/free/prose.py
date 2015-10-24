b5fd575480f7bcceffc8eb4042bc12dfe25d3712
Oscar Byrne <oscar.byrne@gmail.com>
2015-10-24 22:34:49 +0100

    Make ProseFinder use class methods

--- apis/free/prose.py
+++ apis/free/prose.py
@@ -15,10 +15,8 @@ from ..paid import Langdetecter
 
 class ProseFinder(object):
 
-    def __init__(self):
-        self.logger = logging.getLogger("ProseFinder")
-
-    def get_raw_book(self):
+    @staticmethod
+    def get_raw_book():
         while True:
             try:
                 text = load_etext(random.randrange(46000)) #46000 is approximately size of gutenberg catalogue
@@ -27,31 +25,36 @@ class ProseFinder(object):
             else:
                 return strip_headers(text)
 
-    def get_raw_paragraph(self):
-        text = self.get_raw_book()
-        paragraphs = text.split("\n\n") 
+    @classmethod
+    def get_raw_paragraph(cls):
+        text = cls.get_raw_book()
+        paragraphs = text.split("\n\n")
         return paragraphs[len(paragraphs)//2]
 
-    def is_useful_prose(self, text):
-        is_english = Langdetecter().is_english(text.split(".")[0]) #check first sentence
-        #too_much_uppercase
-        #too_much_whitespace
+    @staticmethod
+    def is_useful_prose(text):
         suitable_length = len(text) > 300 and len(text) < 2000
-        self.logger.info("is english: {}; is suitable length: {}".format(is_english, suitable_length))
-        return all([is_english, suitable_length,])
+        if not suitable_length:
+            return False
 
-    def sanitize_text(self, text):
+        is_english = Langdetecter().is_english(text.split(".")[0]) #check first sentence
+        if not is_english:
+            return False
+
+        return True
+
+    @staticmethod
+    def sanitize_text(text):
         text = text.replace("_", "").replace("*", "")   #remove non-standard punctuation
         text = re.sub("\[[^\]]*\] ", "", text)          #remove text contained in square brackets
         text = " ".join(text.split())                   #remove unnecessary whitespace
         return text
 
-    def get_paragraph(self):
+    @classmethod
+    def get_paragraph(cls):
         while True:
-            paragraph = self.get_raw_paragraph()
-            if self.is_useful_prose(paragraph):
-                return self.sanitize_text(paragraph)
+            paragraph = cls.get_raw_paragraph()
+            if cls.is_useful_prose(paragraph):
+                return cls.sanitize_text(paragraph)
             else:
                 pass
-
-
